eb00d9d4b8b18ab9479557514ba38c0f3843f510
Анна Валитова <anavalitova0@gmail.com>
2022-06-04 14:43:10 +1000

    Добавьте файлы проекта.

---
+++ Telegram_Bot/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace Telegram_Bot
+{
+    class Program
+    {
+       
+        
+        static void Main(string[] args)
+        {
+            var token = "5427814426:AAET9AfBU23P4R8DnecRF9R3v3bC1q_A-VY";
+            var bot = new TelegramBotClient(token);
+
+        }
+    }
+}
